0216660daa993d053605ad38a76e7881efcd413e
swamyuvas39 <swamyuvas@gmail.com>
2023-06-27 12:40:33 +0530

    promieses added

--- promises.js
+++ promises.js
@@ -17,36 +17,47 @@ const posts = [
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         posts.push(post);
-        resolve(post);
+
+        const error = false;
+
+        if (!error){
+            resolve();
+        }
+        else{
+            reject('Error: Something went wrong');
+        }
       }, 1000);
     });
   }
-  
-  function updateLastUserActivityTime() {
+   function updateLastUserActivityTime() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         const currentTime = new Date().toLocaleTimeString();
         resolve(currentTime);
       }, 1000);
     });
-  }
+  } 
   
-  function deletePost(post) {
+function deletePost() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        const index = posts.findIndex((p) => p.title === post.title);
-        if (index !== -1) {
-          posts.splice(index, 1);
-          resolve();
+        if (posts.values !== 0) {
+          resolve(posts.pop());
         } else {
-          reject(new Error('Post not found.'));
+          reject('Array is empty now');
         }
-      }, 1000);
+      }, 1000); 
     });
   }
+ 
+  createPost({title:'Post Three', body:'This is post three'})
+  .then(getPosts)
+  .catch(err=>console.log(err))
+  .then(deletePost)
+  .catch(err=>console.log(err));
   
   function updateLastUserActivityTimeAndDeletePost() {
-    Promise.all([createPost({ title: 'Post Three', body: 'This is post three' }), updateLastUserActivityTime()])
+    Promise.all([createPost({ title: 'Post Four', body: 'This is post four' }), updateLastUserActivityTime()])
       .then(([createdPost, activityTime]) => {
         console.log('Posts:', posts);
         console.log('Last Activity Time:', activityTime);
@@ -56,11 +67,11 @@ const posts = [
         console.log('Post deleted successfully.');
         console.log('New Posts:', posts);
       })
-      .catch((error) => {
-        console.error('Error:', error.message);
+      .catch((err) => {
+        console.log(err);
       });
   }
   
   getPosts();
-  updateLastUserActivityTimeAndDeletePost();
+  updateLastUserActivityTimeAndDeletePost(); 
   
